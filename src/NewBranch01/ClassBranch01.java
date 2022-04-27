9ddd52ce71cced7ab6155d49008b2cfa5f9f8a97
unnice <odinok1y@mail.ru>
2022-04-27 22:38:07 +0300

    first branch commit

---
+++ src/NewBranch01/ClassBranch01.java
@@ -0,0 +1,15 @@
+package NewBranch01;
+
+public class ClassBranch01 {
+
+    public static void main(String[] args) {
+
+        firstTask();
+
+    }
+
+    private static void firstTask(){
+        System.out.println("finish first task");
+    }
+
+}
